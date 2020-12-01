0616d1597c66276eb89a01c41b3e7a3aec895236
alkim media - Kerim Alkim <info@alkim.de>
2020-12-01 07:41:16 +0100

    Adminbereich verschönert

--- new_dirs/includes/modules/payment/amazon_pay/lang/german.php
+++ new_dirs/includes/modules/payment/amazon_pay/lang/german.php
@@ -31,6 +31,10 @@ $texts = [
     'APC_CAPTURE_MODE_TITLE'=>'Art des Zahlungseinzugs',
     'APC_ORDER_STATUS_SHIPPED_TITLE'=>'Status f&uuml;r versendete Bestellungen',
 
+    'HEADING_CREDENTIALS_TITLE'=>'Amazon Pay Konto',
+    'HEADING_GENERAL_TITLE'=>'Allgemeine Einstellungen',
+    'HEADING_STYLE_TITLE'=>'Design Einstellungen',
+
     //shop
     'TEXT_AMAZON_PAY_ERROR'=>'Ihre Zahlung war nicht erfolgreich. Bitte verwenden Sie eine andere Zahlungsart',
     'TEXT_AMAZON_PAY_PENDING'=>'Ihre Zahlung mit Amazon Pay ist derzeit noch in Pr&uuml;fung. Bitte beachten Sie, dass wir uns mit Ihnen in K&uuml;rze per Email in Verbindung setzen werden, falls noch Unklarheiten bestehen sollten.'
